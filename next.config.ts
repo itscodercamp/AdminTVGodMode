334f3670fa33656742de2741114a607bee26d1e3
coder camp <itscodercamp@gmail.com>
2025-10-01 07:44:12 +0000

    bhai jb me production pr dalo toh mujhe config file me kuch chanes krne

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,49 @@
 import type {NextConfig} from 'next';
 
+const appUrl = process.env.NEXT_PUBLIC_APP_URL
+  ? new URL(process.env.NEXT_PUBLIC_APP_URL).hostname
+  : '';
+
+const remotePatterns: NextConfig['images']['remotePatterns'] = [
+  {
+    protocol: 'https',
+    hostname: 'placehold.co',
+    port: '',
+    pathname: '/**',
+  },
+  {
+    protocol: 'https',
+    hostname: 'images.unsplash.com',
+    port: '',
+    pathname: '/**',
+  },
+  {
+    protocol: 'https',
+    hostname: 'picsum.photos',
+    port: '',
+    pathname: '/**',
+  },
+  {
+    protocol: 'https',
+    hostname:
+      '9000-firebase-studio-1757611792048.cluster-ancjwrkgr5dvux4qug5rbzyc2y.cloudworkstations.dev',
+    port: '',
+    pathname: '/**',
+  },
+];
+
+if (appUrl) {
+  remotePatterns.push({
+    protocol: 'http',
+    hostname: appUrl,
+  });
+  remotePatterns.push({
+    protocol: 'https',
+    hostname: appUrl,
+  });
+}
+
+
 const nextConfig: NextConfig = {
   /* config options here */
   typescript: {
@@ -9,32 +53,7 @@ const nextConfig: NextConfig = {
     ignoreDuringBuilds: true,
   },
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: 'picsum.photos',
-        port: '',
-        pathname: '/**',
-      },
-      {
-        protocol: 'https',
-        hostname: '9000-firebase-studio-1757611792048.cluster-ancjwrkgr5dvux4qug5rbzyc2y.cloudworkstations.dev',
-        port: '',
-        pathname: '/**',
-      },
-    ],
+    remotePatterns,
   },
 };
 
